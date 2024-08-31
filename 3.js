46c24bc42876698b9e890a84af7c6b7740f41707
saidbmlk <saidbmlk0@gmail.com>
2024-08-31 23:57:07 +0100

    update

---
+++ 3.js
@@ -0,0 +1,19 @@
+
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4851817,4,511,95,18,00000000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+  
+   atOptions = {
+		'key' : '17e3c211e89d5acdc1f9bcf062a5bbdf',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+  document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/17e3c211e89d5acdc1f9bcf062a5bbdf/invoke.js"></scr' + 'ipt>');
